f66b9980c9d20f91f2f3a6fd82792fe9ec1d7d96
S. Whitin <se.whitin@gmail.com>
2025-11-17 11:27:14 -0500

    first overrides are in

---
+++ Cafe.java
@@ -0,0 +1,111 @@
+/* This is a stub for the Cafe class */
+
+import java.util.ArrayList;
+
+public class Cafe extends Building implements CafeRequirements {
+    private int nCoffeeOunces;
+    private int nSugarPackets;
+    private int nCreams;
+    private int nCups;
+
+    /**
+     * Constructor, sets default inventory
+     * @param name string, the name of the building
+     * @param address string, the address of the building
+     * @param nFloors int, the number of floors of the building
+     */
+    public Cafe(String name, String address, int nFloors) {
+        super(name, address, nFloors);
+        this.nCoffeeOunces = 20000;
+        this.nSugarPackets = 1500;
+        this.nCreams = 1500;
+        this.nCups = 2000;
+    }
+
+    /**
+     * turns order into an ArrayList of Integers
+     * @param size int, number of ounces of coffee in order
+     * @param nSugarPackets, int number of sugar packets in order
+     * @param nCreams int, number of creams in order
+     */
+    private ArrayList<Integer> getOrder(int size, int nSugarPackets, int nCreams){
+        ArrayList<Integer> order = new ArrayList<Integer>();
+        order.add(size);
+        order.add(nSugarPackets);
+        order.add(nCreams);
+        int cups = 1;
+        order.add(cups);
+        return order;
+    }
+
+    /**
+     * turns stocked items into an ArrayList of Integers
+     * @param size int, number of ounces of coffee in inventory
+     * @param nSugarPackets, int number of sugar packets in inventory
+     * @param nCreams int, number of creams in inventory
+     */
+    private ArrayList<Integer> getInventory(){
+        ArrayList<Integer> inventory = new ArrayList<Integer>();
+        inventory.add(this.nCoffeeOunces);
+        inventory.add(this.nSugarPackets);
+        inventory.add(this.nCreams);
+        inventory.add(this.nCups);
+        return inventory;
+    }
+    
+    /**
+     * Decreases inventory by amount specified for each paramter and by one cup. If there is not enough left in stock to complete order, stock will be replinished.
+     * @param size int, number of ounces of coffee in order
+     * @param nSugarPackets, int number of sugar packets in order
+     * @param nCreams int, number of creams in order
+     */
+    public void sellCoffee(int size, int nSugarPackets, int nCreams){
+        ArrayList<Integer> order = getOrder(size, nSugarPackets, nCreams);
+        ArrayList<Integer> inventory = getInventory();
+        for(int i = 0; i < inventory.size(); i++){
+            int stock = inventory.get(i);
+            int request = order.get(i);
+            System.out.println(stock+" in relation to "+request);
+            if (stock - request < 0){
+                this.restock(20000, 1500, 1500, 2000);
+            }
+        }
+        this.nCoffeeOunces = this.nCoffeeOunces - size;
+        this.nSugarPackets = this.nSugarPackets - nSugarPackets;
+        this.nCreams = this.nCreams - nCreams;
+        this.nCups = this.nCups - 1;
+    }
+
+    /**
+     * Return stock to the values given as paramters 
+     * @param nCoffeeOunces int, number of coffee ounes to be in stock
+     * @param nSugarPackets int, number of sugar packets to have in stock
+     * @param nCreams int, number of creams to have in stock
+     * @param nCups int, number of cups to have in stock
+     */
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups){
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
+    }
+
+    // Overriding Navigation Methods
+    public void showOptions(){
+        System.out.println("Available options at " + this.name + ":\n + enter() \n + exit()");     
+    }
+
+    public void goToFloor(int floor){
+        throw new RuntimeException("There is only one floor in the cafe.");
+    }
+
+    public static void main(String[] args) {
+        Cafe compass = new Cafe("Compass Cafe", "1 Chapin Way", 3);
+        compass.sellCoffee(19000, 0, 0);
+        System.out.println(compass.nCoffeeOunces);
+        compass.sellCoffee(3000, 0, 0);
+        System.out.println(compass.nCoffeeOunces);
+        System.out.println(compass.nCups);
+    }
+    
+}
---
+++ House.java
@@ -0,0 +1,102 @@
+/* This is a stub for the House class */
+
+import java.util.ArrayList;
+
+public class House extends Building implements HouseRequirements{
+  private ArrayList<Student> residents;
+  // The <Student> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+  private boolean hasElevator;
+
+  /**
+   * Constructor
+   * @param name string, the name of the building
+   * @param address string, the address of the building
+   * @param nFloors int, the number of floors of the building
+   * @param d boolean, if the house has a dining room or not
+   * @param e boolean, if the house has an elevator or not
+   */
+  public House(String name, String address, int nFloors, boolean d, boolean e) {
+    super(name, address, nFloors);
+    this.residents = new ArrayList<Student>();
+    this.hasDiningRoom = d;
+    this.hasElevator = e;
+    System.out.println("You have built a house: 🏠");
+  }
+  
+  /**
+   * Find if house has dining room
+   * @return boolean, if the house has a dining room or not
+   */
+  public boolean hasDiningRoom(){
+    return this.hasDiningRoom;
+  }
+  
+  /**
+   * Get the number of residents in the house
+   * @return number of residents 
+   */
+  public int nResidents(){
+    return this.residents.size();
+  }
+  
+  /**
+   * Move in a student, or add them to the ArrayList of students
+   * @param s the student moving in
+   */
+  public void moveIn(Student s){
+    this.residents.add(s);
+  }
+  
+  /**
+   * Move out a student, or remove them from the ArrayList of students
+   * @param s the student moving out
+   * @return the student moving out
+   */
+  public Student moveOut(Student s){
+    this.residents.remove(s);
+    return s;
+  }
+  
+  /**
+   * see if student lives in house
+   * @param s student to find resident status of
+   * @return if student is a resident
+   */
+  public boolean isResident(Student s){
+    boolean livesHere = this.residents.contains(s);
+    return livesHere;
+  }
+
+// Overriding Navigation Methods
+public void showOptions() {
+  if (this.hasElevator == true){
+    System.out.println("Available options at " + this.name + ":\n + enter() \n + exit() \n + goUp() \n + goDown()\n + goToFloor(n)");
+  }else{
+    System.out.println("Available options at " + this.name + ":\n + enter() \n + exit()");
+  }
+}
+
+public void goToFloor(int floorNum){
+  if(this.hasElevator == true){
+    this.activeFloor = floorNum;
+  }else{
+    throw new RuntimeException("There is only one floor in the cafe.");
+  }
+}
+  public static void main(String[] args) {
+    House wilson = new House("Wilson", "1 Paradise", 5, true, false);
+    System.out.println(wilson.hasDiningRoom());
+    Student anna = new Student("anna", null, 2026);
+    Student sox = new Student("sox", null, 2027);
+    wilson.moveIn(anna);
+    wilson.moveIn(sox);
+    System.out.println(wilson.nResidents());
+    System.out.println(wilson.isResident(sox));
+    System.out.println(wilson.isResident(anna));
+    wilson.moveOut(sox);
+    System.out.println(wilson.isResident(sox));
+    System.out.println(wilson.nResidents());
+  }
+
+}
